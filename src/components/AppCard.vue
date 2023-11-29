1435a30c66dbfb0f475ea4e41ea98d8830d8b272
PapiMatteo <matteopapi.work@gmail.com>
2023-11-29 14:50:29 +0100

    add: languages flag for some language implemente

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,18 @@
 <script>
 export default {
+    data() {
+        return {
+            langImg: ['en', 'it', 'es']
+        }
+    },
     props: {
         movie: Object
+    },
+    methods: {
+        getImagePath(img) {
+            console.log(img);
+            return new URL(`../assets/img/${img}.jpg`, import.meta.url).href;
+        }
     }
 }
 </script>
@@ -10,7 +21,8 @@ export default {
     <div class="card">
         <h3>{{ movie.title }}</h3>
         <p>{{ movie.original_title }}</p>
-        <p>{{ movie.original_language }}</p>
+        <img :src="getImagePath(movie.original_language)" alt="" v-if="langImg.includes(movie.original_language)">
+        <p v-else>{{ movie.original_language }}</p>
         <p>{{ movie.vote_average }}</p>
     </div>
     
@@ -20,6 +32,11 @@ export default {
 
 .card {
     margin-bottom: 1rem;
+    img {
+        width: 30px;
+        height: 15px;
+
+    }
 }
 
 </style>
